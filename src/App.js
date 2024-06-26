9a3abf5e858b96afdd326ce14302301e400555dd
Eldar404 <g0nnasul005@gmail.com>
2024-06-26 21:23:36 +0400

    Проект магазин
    Работа с джейсон,сделали при нажатии на карточку затемнение

--- src/App.js
+++ src/App.js
@@ -3,112 +3,27 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Items from "./components/Items";
 import Category from "./components/Category";
+import ShowFullItem from "./components/ShowFullItem";
+import axios from "axios";
 
 function App() {
-  const [items,setItems]=useState([
-    {
-      id:1,
-      title:'Bmw m4',
-      img:'bmwm44.webp',
-      desc:'Комплектация  Competition 3.0 AT 510 л.с. автомат.',
-      category:'sedan',
-      price:'18990'
-    },
-    {
-      id:2,
-      title:'Mercedes e300',
-      img:'e300cab.png',
-      desc:'E-класса, полный привод. Автомат. Гибридный двигатель мощностью 585 лошадиных сил.',
-      category:'cabrio',
-      price:'19990'
-    },
-  {
-    id:3,
-      title:'Lamborghini URUS',
-      img:'lamburus.jpg',
-      desc:'',
-      category:'jeep',
-      price:'29990'
-  },
-  {
-    id:4,
-      title:'Range-Rover Velar',
-      img:'velar.jpeg',
-      desc:'',
-      category:'jeep',
-      price:'17990'
-  },
-    {
-  id:5,
-      title:'Mercedes G',
-      img:'Gclass.webp',
-      desc:'',
-      category:'jeep',
-      price:'17990'
-    },
-    { id:6,
-      title:'Audi R8',
-      img:'r8.jpeg',
-      desc:'',
-      category:'sedan',
-      price:'20990'
-     },
-     {
-      id:7,
-        title:'Mercedes c63',
-        img:'mercedesc64.webp',
-        desc:'',
-        category:'sedan',
-        price:'17990'
-    },
-    {
-      id:8,
-        title:'Bmw m5',
-        img:'bmwm5.jpg',
-        desc:'',
-        category:'sedan',
-        price:'21990'
-    },
-    {
-      id:9,
-        title:'Bmw x5m',
-        img:'x5m.jpg',
-        desc:'',
-        category:'jeep',
-        price:'20990'
-    },
-    ,
-    {
-      id:10,
-      title:'Mercedes sl',
-      img:'sl.jpg',
-      desc:'',
-      category:'cabrio',
-      price:'16990'
-    },
-    {
-      id:11,
-      title:'bmw m2',
-      img:'bmwm2cab.jpg',
-      desc:'',
-      category:'cabrio',
-      price:'23990'
-    },
-    {
-      id:12,
-      title:'Lamborghini huracan',
-      img:'lambhurcab.png',
-      desc:'',
-      category:'cabrio',
-      price:'38990'
-    }
-
-  ]);
+  const [items,setItems]=useState([]);
   const [orders,setOrders]=useState([]);
   const [currentItems,setCurrentItems]=useState([]);
+
   useEffect(()=>{
-    setCurrentItems(items);
-  },[items]);
+    axios
+      .get("http://localhost:3001/items")
+      .then((response)=>{
+        setItems(response.data)
+        chooseCategory("all")
+        setCurrentItems(response.data)
+      })
+      .catch((error)=>{
+        console.error("Ошибка при загрузке данных!:",error)
+      })
+  },[]);
+
 const chooseCategory=(category)=>{
   if(category==="all"){
     setCurrentItems(items);
@@ -127,11 +42,20 @@ const chooseCategory=(category)=>{
   const deleteOrder=(id)=>{
   setOrders(orders.filter((el)=>el.id!==id));
   }
+const [showFullItem,setShowFullItem]=useState(false);
+const [fullItem,setFullItem]=useState({});
+const onShowItem= (item) =>{
+  setFullItem(item);
+  setShowFullItem(!showFullItem);
+} 
+
   return (
     <div className="wrapper">
     <Header orders={orders} onDelete={deleteOrder}/>
     <Category chooseCategory={chooseCategory}/>
-    <Items allItems={currentItems} onAdd={addToOrder} />
+    <Items allItems={currentItems} onShowItem={onShowItem} onAdd={addToOrder} />
+    {showFullItem && <ShowFullItem onShowItem={onShowItem} onAdd={addToOrder}
+    item={fullItem}/>}
     <Footer/>
     </div>
     
--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -4,10 +4,10 @@ import styles from "./Item.module.scss";
 export default function Item(props){
     return(
         <div className={styles.item}>
-            <img src={"./images/"+props.item.img}/>
-    <h2>{props.item.title}</h2>
-    <p>{props.item.desc}</p>
-    <b>{props.item.price}₽</b>
+        <img src={"./images/"+props.item.img} onClick={()=>props.onShowItem(props.item)}/>
+        <h2>{props.item.title}</h2>
+        <p>{props.item.desc}</p>
+        <b>{props.item.price}₽</b>
             <div className={styles.addToCard} onClick={()=>props.onAdd(props.item)}>+</div>
         </div>
     );
